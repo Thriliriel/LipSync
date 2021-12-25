b7de0ba73f858107fb06cb06086847a8c550cf38
Thriliriel <wsk.shadow@gmail.com>
2021-12-25 14:48:51 -0300

    Lip Sync asset.

---
+++ Assets/LipSync.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LipSync : MonoBehaviour
+{
+    //is jaw, or blendshape?
+    public bool isJaw;
+
+    //audio stuff
+    int volume = 40;
+    int frqLow = 200;
+    int frqHigh = 800;
+    int fMax = 24000;
+
+    private float y0;
+    public AudioSource audio;
+    public int bsIndex;
+    private float[] freqData;
+    private int nSamples;
+    private int qSamples = 0;
+    private int posFilter = 0;
+    private int sizeFilter = 5;
+    private float[] filter;
+    private float filterSum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nSamples = 256;
+        audio = GetComponent<AudioSource>(); // get AudioSource component
+
+        //if it is jaw, get the initial scale
+        if (isJaw)
+        {
+            y0 = transform.localScale.y;
+        }
+        else //otherwise, get the initial BS value (in our case, it is the index 24 of the blendshapes, put yours)
+        {
+            y0 = GetComponent<SkinnedMeshRenderer>().GetBlendShapeWeight(bsIndex);
+        }
+        freqData = new float[nSamples];
+
+        //maybe you are already playing, so just comment this
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if it is jaw, just change the scale of the mouth
+        if (isJaw)
+        {
+            //float newY = y0 + BandVol(frqLow, frqHigh) * (volume/4);
+            float newY = y0 - MovingAverage(BandVol(frqLow, frqHigh)) * volume / 4;
+            transform.localScale = new Vector3(transform.localScale.x, newY, transform.localScale.z);
+        }//otherwise, use the BS
+        else
+        {
+            GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(bsIndex, y0 + BandVol(frqLow, frqHigh) * volume * 10);
+        }
+    }
+
+    private float BandVol(float fLow, float fHigh)
+    {
+        fLow = Mathf.Clamp(fLow, 20, fMax); // limit low...
+        fHigh = Mathf.Clamp(fHigh, fLow, fMax); // and high frequencies
+        // get spectrum: freqData[n] = vol of frequency n * fMax / nSamples
+        audio.GetSpectrumData(freqData, 0, FFTWindow.BlackmanHarris);
+        int n1 = (int)Mathf.Floor(fLow * nSamples / fMax);
+        int n2 = (int)Mathf.Floor(fHigh * nSamples / fMax);
+        float sum = 0;
+        // average the volumes of frequencies fLow to fHigh
+        for (var i = n1; i <= n2; i++)
+        {
+            sum += freqData[i];
+        }
+        return sum / (n2 - n1 + 1);
+    }
+
+    private float MovingAverage(float sample)
+    {
+        if (qSamples == 0) filter = new float[sizeFilter];
+        filterSum += sample - filter[posFilter];
+        filter[posFilter++] = sample;
+        if (posFilter > qSamples) qSamples = posFilter;
+        posFilter = posFilter % sizeFilter;
+        return filterSum / qSamples;
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+This asset contains a simple script to emulate a lip sync for a virtual agent. Why did i do that? Because of the lack of free lip sync assets for Unity, but of course! I got most of this code online and adjusted to what i needed for. Then, tried to make it the most general possible, so it would be easy to integrate for others. It may not be the best or the most accurate lip sync tool, but it is free and easy to use =)
+
+HOW TO USE:
+
+Download and import the asset into your project. You will see that it counts with a LipSync.cs script. Add this script as a component of the GameObject that you want to use. Fill the fields that you will need and you are done. Pretty simple uh? Next, i present some important details of this configuration.
+
+- In which gameobject should i add the script component? It will depends on the mesh you have as your virtual agent, especially concerning the mouth. In general, there are two ways used to animate mouths: Jaw Movement and BlendShapes. Jaw Movement is the simplest one, since it only needs the mouth itself. This asset counts with a virtual agent based on Jaw Movement, so you can use it as an example. Blendshapes are more complex, but deliver a better and more fluid animation. If your agent uses Blendshapes, you should add the LipSync script as a component of the object which contains the definition of the blendshapes. Otherwise, if you have a simple jaw, you should add the LipSync script as a component of the mouth object (in this case, you can use the agent of this asset as an example).
+
+- The gameobject which receives the script must have an AudioSource component, because it is from where it gets the audio used for the lipSync. For the blendshapes, it also needs to have a SkinnedMeshRenderer with the blendshapes.
+
+- When the script is added, you can see that it has some parameters. If your model is based on Jaw Movement, you only need to mark the Is Jaw ticket. If your model is based on blendshapes, untick the Is Jaw ticket and inform the index of the blendshape which deals with the mouth movement, in Bs Index. 
+
+That is it. If you have any question or whatever, just send a message =)
